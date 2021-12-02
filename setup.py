2ed988a5bfd4cb68e72ee9c4f036ce56ed8781c5
Iason Sarantopoulos <iason@sarantop.com>
2021-12-02 13:16:30 +0200

    Remove unecessary dep

--- setup.py
+++ setup.py
@@ -5,6 +5,4 @@ setup(name='bridging_the_gap',
       version='0.1',
       author='Iason Sarantopoulos',
       author_email='iasons@auth.gr',
-      install_requires=['clt_core @ git+ssh://git@github.com/robot-clutter/clt_core@main#egg=clt_core',
-      ]
 )
